f060b90b098072d4ddbf670586f75c88a19f4178
vegerot12 <nandhinirajavell@gmail.com>
2023-09-03 11:00:43 +0530

    Time: 91 ms (64.08%) | Memory: 96.1 MB (69.51%) - LeetSync

--- 463-island-perimeter/island-perimeter.cpp
+++ 463-island-perimeter/island-perimeter.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int islandPerimeter(vector<vector<int>>& grid) {
-        int r = grid.size(), c= grid[0].size();
-        int res = 0;
-
-        for(int i =0;i<r;i++){
+        int res =0, r= grid.size(), c= grid[0].size();
+        for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
                 if(grid[i][j] == 1){
-                    res += 4;
-                    if(i>0 && grid[i-1][j] ==1)
-                    res -=2;
-                    if(j>0 && grid[i][j-1] ==1)
-                    res-=2;
+                    res+= 4; // first add 4 ( assuming all 4 are water )
+                    if( i>0 && grid[i-1][j] == 1 ) //  check if up is a land then subtract 2 - one for this cell's upper bundayr and one for that cells' down boundary
+                    res -= 2;
+ // check if left is a land then subtract 2 - one for this cell's left bundayr and one for that cells' right  boundary
+                    if(j>0 && grid[i][j-1] ==1 ) res -= 2;
                 }
+
             }
         }
         return res;

bfe37e1b1106adcd6a530c6cd678d4a425040cba
vegerot12 <nandhinirajavell@gmail.com>
2023-09-13 18:45:49 +0530

    Time: 332 ms (96.66%) | Memory: 83.8 MB (49.54%) - LeetSync

--- 2390-naming-a-company/naming-a-company.cpp
+++ 2390-naming-a-company/naming-a-company.cpp
@@ -10,7 +10,7 @@ public:
          }
 
 
-        for(int i=0;i<25;i++){
+        for(int i=0;i<26;i++){
             // fpr each in set i with each in other 25 sets 
             for(int j=i+1;j<26;j++){
                 long long mutual=0;
@@ -18,29 +18,12 @@ public:
                     if(s[i].find(p) != s[i].end())
                     mutual++;
                 }
-            //     
                
-                cnt+= ((s[i].size()-mutual )*( s[j].size() - mutual ));
-                // cnt +=  ((s[i].size() -mutual) * (s[j].size()-mutual));
+                cnt+= ((s[i].size()-mutual )*( s[j].size() - mutual));
 
 
             }
         }
-        
-    // for(int i=0;i<25;i++){
-    //         for(int j=i+1;j<26;j++){
-    //            // find mutual
-    //            int mutual =0;
-    //            for(auto& p: s[j]){
-    //                 if(s[i].find(p) != s[i].end())
-    //                 mutual++;
-    //            }
-    //             // find valid names
-    //             // each pair of valid suffix will lead to two names- ab and ba  so 2* cnt
-    //             cnt += 2* ((s[i].size() -mutual) * (s[j].size()-mutual));
-
-    //         }
-    //     }
-return cnt*2;
+        return cnt*2;
     }
 };

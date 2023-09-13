e907229a97081419ff95adc94a9c4fa98bf30607
vegerot12 <nandhinirajavell@gmail.com>
2023-09-13 13:15:00 +0530

    Time: 74 ms (84.35%) | Memory: 39.7 MB (94.76%) - LeetSync

--- 816-design-hashset/design-hashset.cpp
+++ 816-design-hashset/design-hashset.cpp
@@ -1,87 +1,21 @@
-// class Bucket{
-// private:
-// vector<int> bucketList;
-
-// public:
-// bool contains(int key){
-//     return find(  bucketList.begin(), bucketList.end(), key ) != end(bucketList);
-// }
-// void add(int x){
-//     if(!contains(x))
-//     bucketList.push_back(x);
-// }
-
-// void remove(int x){
-   
-//     auto pos = find(bucketList.begin(), bucketList.end(),x) ;
-//     if(pos != bucketList.end())
-//     bucketList.erase(pos);
-// }
-
-// };
-
-
-// class MyHashSet {
-//     private: 
-//     int hash;
-//     vector<Bucket> buckets; // each bucket will have a list 
-// public:
-//     MyHashSet(int size=137) {
-//         hash = size;
-//         buckets.resize(size);
-//     }
-    
-//     void add(int key) {
-//         int x = key%hash; // we hash it
-//         buckets[x].add(key); // put it in hashed list 
-//     }
-    
-//     void remove(int key) {
-//          int x = key%hash;
-//          if(contains(x))
-//          buckets[x].remove(key);
-
-        
-//     }
-    
-//     bool contains(int key) {
-//          int x = key%hash;
-//          return buckets[x].contains(key);
-//     }
-// };
-
-// /**
-//  * Your MyHashSet object will be instantiated and called as such:
-//  * MyHashSet* obj = new MyHashSet();
-//  * obj->add(key);
-//  * obj->remove(key);
-//  * bool param_3 = obj->contains(key);
-//  */
-/*
-TC: O(n) and amortised = O(1)
-SC : O(n)
-*/
 class Bucket{
-
 private:
 vector<int> bucketList;
 
 public:
-// to add 
-void add(int key){
-if(!contains(key))
-bucketList.push_back(key);
+bool contains(int key){
+    return find(  bucketList.begin(), bucketList.end(), key ) != bucketList.end();
 }
-// to check if the list alrd contains key - returns true if its contains key else false 
-bool  contains(int key){
-   return find(begin(bucketList), end(bucketList), key) != end(bucketList);
+void add(int x){
+    if(!contains(x))
+    bucketList.push_back(x);
 }
-// to remove 
-void remove(int key){
-  
-auto pos = find(begin(bucketList), end(bucketList), key);
-if(pos!= end(bucketList))
-bucketList.erase(pos);
+
+void remove(int x){
+   
+    auto pos = find(bucketList.begin(), bucketList.end(),x) ;
+    if(pos != bucketList.end())
+    bucketList.erase(pos);
 }
 
 };
@@ -120,8 +54,46 @@ int h = hash(key);
 return buckets[h].contains(key);
 
 }
-
 };
+// /**
+//  * Your MyHashSet object will be instantiated and called as such:
+//  * MyHashSet* obj = new MyHashSet();
+//  * obj->add(key);
+//  * obj->remove(key);
+//  * bool param_3 = obj->contains(key);
+//  */
+/*
+TC: O(n) and amortised = O(1)
+SC : O(n)
+*/
+// class Bucket{
+
+// private:
+// vector<int> bucketList;
+
+// public:
+// // to add 
+// void add(int key){
+// if(!contains(key))
+// bucketList.push_back(key);
+// }
+// // to check if the list alrd contains key - returns true if its contains key else false 
+// bool  contains(int key){
+//    return find(begin(bucketList), end(bucketList), key) != end(bucketList);
+// }
+// // to remove 
+// void remove(int key){
+  
+// auto pos = find(begin(bucketList), end(bucketList), key);
+// if(pos!= end(bucketList))
+// bucketList.erase(pos);
+// }
+
+// };
+
+
+
+// };
 
 
 /**

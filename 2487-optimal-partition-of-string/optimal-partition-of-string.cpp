ffa0234b50e87fa18bf68b03db0c1e740b8951c9
vegerot12 <nandhinirajavell@gmail.com>
2023-09-14 18:52:58 +0530

    Time: 26 ms (79.22%) | Memory: 10.8 MB (64.15%) - LeetSync

--- 2487-optimal-partition-of-string/optimal-partition-of-string.cpp
+++ 2487-optimal-partition-of-string/optimal-partition-of-string.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int partitionString(string s) {
-        unordered_map<char,int> m;
+        vector<int> m(26,0);
         int start =0, res =1; // initially 1 string so res starts at 1
         for(int i=0;i<s.size();i++){
-            if(m[s[i]]-1>=start)
+            if(m[s[i]-'a']-1>=start)
             {
                 res++;
                 start = i;
-                m[s[i]] = i+1;
                 }
-            m[s[i]]=i+1;
+            m[s[i]-'a']=i+1;
             
         }
         return res;

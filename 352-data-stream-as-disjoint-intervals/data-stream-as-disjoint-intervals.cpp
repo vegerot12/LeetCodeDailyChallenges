e015cf6e7d9b26e1f36335d6675327ebdecb1574
vegerot12 <nandhinirajavell@gmail.com>
2023-09-15 13:57:55 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (19.51%) - LeetSync

--- 352-data-stream-as-disjoint-intervals/data-stream-as-disjoint-intervals.cpp
+++ 352-data-stream-as-disjoint-intervals/data-stream-as-disjoint-intervals.cpp
@@ -1,28 +1,37 @@
 class SummaryRanges {
-    private:
-    map<int,bool> intr ; // tree Map
+    vector<vector<int>> res;
 public:
     SummaryRanges() {
         
     }
     
     void addNum(int value) {
-        intr[value] = true;
+        vector<int> newInterval = {value, value};
+        int i =0,overlap=0;
+        int n = res.size();
+        // for lopp exit will be when we crossed overlapping section 
+        for(i=0;i<n;i++){
+            // new interval ending is smaller than cur interval starting , we passed overlapping or consective  section
+            if(newInterval[1] +1< res[i][0]) break;
+            // if new interval started before cur interval ending 
+            if( newInterval[0]-1 <= res[i][1]){
+                overlap++;
+                newInterval[0] = min(newInterval[0] , res[i][0]);
+                newInterval[1] = max(newInterval[1] , res[i][1]);
+
+
+            }
+
+        }
+        if(overlap>0){
+            res.erase(res.begin()+(i-overlap) , res.begin()+i );
+
+        }
+        res.insert(res.begin()+i-overlap , newInterval);
+        
     }
     
     vector<vector<int>> getIntervals() {
-        vector<vector<int>> res;
-        for(auto itr: intr){
-          
-
-           if( !res.empty() && res.back()[1] + 1 == itr.first){
-                res.back()[1] = itr.first;
-            }
-            else{
-            res.push_back({itr.first, itr.first});
-
-            }
-        }
         return res;
     }
 };
@@ -33,3 +42,4 @@ public:
  * obj->addNum(value);
  * vector<vector<int>> param_2 = obj->getIntervals();
  */
+ 

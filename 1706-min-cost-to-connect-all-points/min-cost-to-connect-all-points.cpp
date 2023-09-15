02802c0a1e5a12341f9999605883524288cb3b99
vegerot12 <nandhinirajavell@gmail.com>
2023-09-15 16:24:25 +0530

    Time: 2007 ms (6.89%) | Memory: 105.8 MB (45.89%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -1,31 +1,74 @@
-
-class Solution {
+class DSU {
+    private:
+    vector<int> parent;
+    vector<int> size;
+    int con =0;
     public:
-int find(vector<int> &ds, int i) {
-    return ds[i] < 0 ? i : ds[i] = find(ds, ds[i]);
-}
-int minCostConnectPoints(vector<vector<int>>& ps) {
-    int n = ps.size(), res = 0;
-    vector<int> ds(n, -1);
-    vector<array<int, 3>> arr;
-    for (auto i = 0; i < n; ++i)
-        for (auto j = i + 1; j < n; ++j) {
-            arr.push_back({abs(ps[i][0] - ps[j][0]) + abs(ps[i][1] - ps[j][1]), i, j});
+    DSU(int n){
+        parent.resize(n);
+        for(int i=0;i<n;i++)
+        parent[i] = i;
+        size.resize(n,1);
+        con =n;
+    }
+    int findParent(int x){
+        if(parent[x] ==x) return x;
+        return parent[x] = findParent(parent[x]);
+    }
+    bool unionFind(int x,int y){
+        int px = findParent(x);
+        int py = findParent(y);
+
+        if(px == py)
+        return false;
+
+        if(size[px] < size[py]){
+            size[py] += size[px];
+            parent[px] = py;
+        }else{
+           size[px] += size[py];
+            parent[py] = px; 
         }
-    make_heap(begin(arr), end(arr), greater<array<int, 3>>());
-    while (!arr.empty()) {
-        pop_heap(begin(arr), end(arr), greater<array<int, 3>>());
-        auto [dist, i, j] = arr.back();
-        arr.pop_back();
-        i = find(ds, i), j = find(ds, j);
-        if (i != j) {
-            res += dist;
-            ds[i] += ds[j];
-            ds[j] = i;
-            if (ds[i] == -n)
-                break;
+        return true;
+    }
+    bool isConnected(){
+        return con==1;
+    } 
+
+    
+};
+class Solution {
+
+public:
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        
+
+int n = points.size(), res=0;
+DSU dsu(n);
+// priority_queue<array<int,3>> pq; // dis, i, j
+vector<array<int,3>> pq;
+for(int i=0;i<n;i++){
+    for(int j=0;j<n;j++){
+        if(i!=j){
+            pq.push_back({ -(abs(points[i][0] - points[j][0]) +abs(points[i][1] - points[j][1] )), i, j});
         }
     }
-    return res;
+
 }
+make_heap(pq.begin(), pq.end());  // O(n) heapify
+
+// k times to pop k ele from heap - k log n
+while(!pq.empty() && !dsu.isConnected() ){
+    // auto p = pq.top();
+    pop_heap(begin(pq), end(pq));
+    auto p = pq.back();
+
+    pq.pop_back();
+    if(dsu.unionFind(p[1], p[2])){
+        res+= -p[0];
+
+    }
+}
+return res;
+    }
 };

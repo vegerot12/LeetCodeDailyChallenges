503ebfc136f0788fb6f34a979c3befcf8e6704cd
vegerot12 <nandhinirajavell@gmail.com>
2023-09-17 17:57:06 +0530

    Time: 152 ms (71.02%) | Memory: 78.9 MB (47.23%) - LeetSync

--- 883-car-fleet/car-fleet.cpp
+++ 883-car-fleet/car-fleet.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
-        vector<pair<int, int>> fleet;
+        vector<pair<int, float>> fleet;
         int n = speed.size();
         for(int i=0;i<n;i++){
-            fleet.push_back({ position[i], speed[i] });
+            fleet.push_back({ position[i],float(target-position[i])/ speed[i] });
         }
 
 
@@ -12,18 +12,20 @@ public:
         sort(fleet.begin() , fleet.end());
 
         int i =n-1;
-        stack<float> s;
+        float prev = 0.0;
+        int cnt = 0;
         while(i>=0){
-            float time =float(target-fleet[i].first)/ fleet[i].second ; // dis / speed
-             if(s.empty()) // if stack empty add to stack
-                s.push(time);
+            
 
-            if(!s.empty() && s.top() < time){ // if its gonna take more time then it cant merge so add in stack
-            s.push(time);
-
+            if(fleet[i].second > prev){ // if its gonna take more time than prev then it cant merge so add count 
+            prev = fleet[i].second ; 
+             cnt++;
             }
+            
             i--;
+
+            
         }
-        return s.size();
+        return cnt;
     }
 };

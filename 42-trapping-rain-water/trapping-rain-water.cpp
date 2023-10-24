1bf1052440b2d8691b26f11c3026625f31d4f30c
vegerot12 <nandhinirajavell@gmail.com>
2023-10-24 23:16:31 +0530

    Time: 8 ms (79.75%) | Memory: 20.1 MB (65.95%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,24 +1,50 @@
 class Solution {
 public:
-    int trap(vector<int>& height) {
-        stack<int> s;
-        int n = height.size(), i=0,res=0;
+    int trap(vector<int>& h) {
+        // stack<int> s;
+        // int n = height.size(), i=0,res=0;
 
-        while(i<n){
+        // while(i<n){
 
-            while(!s.empty() && height[s.top()] < height[i]){
-                int top = s.top();
-                s.pop();
+        //     while(!s.empty() && height[s.top()] < height[i]){
+        //         int top = s.top();
+        //         s.pop();
                 
-                int w =  s.empty() ? i :  i - s.top()-1;
-                int h =    s.empty() ? 0: min(height[s.top()], height[i]) - height[top];
-                res+= w* h;
+        //         int w =  s.empty() ? i :  i - s.top()-1;
+        //         int h =    s.empty() ? 0: min(height[s.top()], height[i]) - height[top];
+        //         cout<<"popped is"<<height[top]<<"top is "<<(s.empty() ? 0: height[s.top()])<<"cur is"<<height[i]<<" "<<w<<" "<<h<<" "<<w*h<<endl;
+        //         res+= w* h;
+        //         cout<<"res"<<res<<endl;
                 
 
+        //     }
+        //    s.push(i);
+        //    i++;
+        // }
+        // return res;
+
+        int res = 0;
+        int n = h.size();
+
+        if(n<=2) return 0;
+
+        int l =0, lm = h[0], r = n-1,rm = h[n-1];
+
+        while(l<=r){
+            if(lm < rm){
+                if(lm <= h[l]){
+                    lm = h[l++];
+                }
+                else res+= lm-h[l++];
             }
-           s.push(i);
-           i++;
+            else{
+                if(rm <= h[r]){
+                    rm = h[r--];
+                }
+                else res+= rm - h[r--];
+            }
         }
+
         return res;
     }
 };

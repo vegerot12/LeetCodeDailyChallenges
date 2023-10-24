9c82cb307cb123781ffafbdc1d311783885d6438
vegerot12 <nandhinirajavell@gmail.com>
2023-10-24 17:22:26 +0530

    Time: 16 ms (60.96%) | Memory: 8.4 MB (25.45%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,49 +1,40 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        // 
-        int n1= s.size() , n2 = t.size();
-        if(n1<n2) return "";
         unordered_map<char, int> m;
-        for(auto tt:t){
-            m[tt]++;
-        }
 
-        int st=0, e=0, len = n1+1, tot = n2, bestStart=0;
+        for(auto c:t){
+            m[c]++;
+        }
+        int n1= s.size(), n2 = t.size();
+        int st=0, e=0, bs=0, len = n1+1 , tot = n2;
 
         while(e<n1){
-            // find winodw move e 
-             
-              if(m.find(s[e])!= m.end()){ // if char at e is in p
-            m[s[e]]--;
-            if(m[s[e]]>=0){
-               
-                tot--;
+            
+            if(m.find(s[e])!= m.end()){
+                if(m[s[e]]>0) tot--;
+                m[s[e]]--;
+                
             }
-              }
-            // if we got window optimsie it 
-            while(tot == 0){
-                if(m.find(s[st]) !=m.end()){
-                    if(len > e-st+1){
-                    len = e-st+1;
-                    bestStart = st;
-                }
-                 if(m[s[st]]==0){
-                        
 
-                   
-                    tot++;
-                }
-                  m[s[st]]++;
-                }
+            while(tot == 0 ){
+          
+                if(m.find(s[st])!= m.end()){
+                    if(len> e-st+1){
+                         len = e-st+1;
+                         bs = st;
+                    }
+                
+                if(m[s[st]] == 0) tot++;
+                m[s[st]]++;
                
-               
-               
-                st++;
             }
+             st++;
+            }
+
             e++;
         }
         if(len > n1) return "";
-        return s.substr(bestStart,len);
+        return s.substr(bs, len);
     }
 };

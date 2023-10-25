a2c058da00cc68c4b4d1fa0db94f65bb2292aa2c
vegerot12 <nandhinirajavell@gmail.com>
2023-10-25 17:15:04 +0530

    Time: 118 ms (85.88%) | Memory: 77.6 MB (74.17%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -11,8 +11,8 @@ public:
             while(!s.empty() && h[s.top()] > ch){
                 int t = s.top();
                 s.pop();
-                int width  =  s.empty() ? i : i- s.top()-1;
-                int area = width * h[t];
+                int width  =  s.empty() ? i : i- s.top()-1; // cur - top of stack will include top of stack also we need inbetween so -1
+                int area = width * h[t];// t index height will be the height that is popped last and also smallest of all the popped heights as teh stack is in increasing order so we need smallest to form rectange 
                 res = max(res,area);
                 
             }

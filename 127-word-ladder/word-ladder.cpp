3ec77777fcbcf69dc7026adc50d441d9646e8802
vegerot12 <nandhinirajavell@gmail.com>
2023-10-25 15:24:44 +0530

    Time: 567 ms (27.95%) | Memory: 15.8 MB (75.42%) - LeetSync

---
+++ 127-word-ladder/word-ladder.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int ladderLength(string begin, string e, vector<string>& wordList) {
+        unordered_set<string> dict;
+
+        for(auto w:wordList){
+            dict.insert(w);
+        }
+        if(dict.find(e) == dict.end()) return 0;
+
+        queue<string> q;
+        q.push(begin);
+
+        int steps =0;
+
+        while(!q.empty()){
+            int s = q.size();
+            
+steps++;
+            while(s--){
+                auto p = q.front();
+                q.pop();
+
+                if(p == e) return steps;
+
+                for(int i = 0 ;i<p.size();i++){
+                    string tmp = p;
+                    for(char c = 'a';c<='z';c++){
+                        tmp[i] = c;
+                            cout<<tmp<<endl;
+                        if(dict.find(tmp) != dict.end()){
+                            cout<<"found"<<tmp<<endl;
+                            q.push(tmp);
+                            dict.erase(tmp);
+                        }
+
+                    }
+                }
+
+            }
+           
+        }
+        return 0;
+    }
+};

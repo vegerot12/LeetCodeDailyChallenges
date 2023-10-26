e2ca10ece30e8b083a5aa5b76bf4f7a86d8ad524
vegerot12 <nandhinirajavell@gmail.com>
2023-10-26 12:40:40 +0530

    Time: 13 ms (61.45%) | Memory: 17.4 MB (42.25%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,67 +1,25 @@
 class Solution {
 public:
-// 
-    vector<vector<int>> insert(vector<vector<int>>& nums, vector<int>& newNums) {
-    //  int ns = newInterval[0], ne = newInterval[1], n = intervals.size(), i=0;
-    //   vector<vector<int>> res;
-     
-    //      while( i< n &&intervals[i][1] < ns) { // before new int
-    //          res.push_back(intervals[i++]);
-    //      }
-    //      while( i<n && intervals[i][0] <= ne){ // after new int , push rest of intr into res
-    //         ns = min(ns, intervals[i][0]);
-    //          ne = max(ne, intervals[i++][1]);
-
-           
-    //      }
-    //       res.push_back({ns, ne});
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+         vector<vector<int>>  res;
+         int n = intervals.size();
          
-    //       if(n>0)
-    //         res.insert(res.end(), intervals.begin() + i, intervals.end());
-    //         return res;
+         int i =0;
 
-         
-     int n=nums.size();
-        if(n==0)
-        {
-            return {newNums};
-        }
+      // before
+             while(i<n &&  intervals[i][1]  < newInterval[0]){
+                 res.push_back(intervals[i++]);
+             }
+      // during       
+             while(i<n && max(intervals[i][0], newInterval[0]) <= min(intervals[i][1], newInterval[1])) {
+                 newInterval[0] = min(intervals[i][0], newInterval[0]);
+                 newInterval[1] = max(intervals[i][1], newInterval[1]);
+                 i++;
+             }
+             res.push_back(newInterval);
+// after
+             while(i<n)res.push_back(intervals[i++]);
        
-        bool f=0;
-        for(int i=0;i<n;i++)
-        {
-            if(newNums[0]<nums[i][0] and newNums[1]<nums[i][0])
-            {
-                nums.insert(nums.begin()+i,newNums);
-                f=1;
-                break;
-            }
-            else
-            if(nums[i][0]<=newNums[0] && newNums[0]<=nums[i][1]
-            ||newNums[0]<=nums[i][0] &&  nums[i][0]<= newNums[1])
-            {
-                int s=i,e=i;
-                while(i<n and newNums[1]>=nums[i][0])
-                {
-                    e=i;
-                    i++;
-                }
-                newNums[0]=min(nums[s][0],newNums[0]);
-                newNums[1]=max(nums[e][1],newNums[1]);
-                nums.erase(nums.begin()+s,nums.begin()+i);
-                nums.insert(nums.begin()+s,newNums);
-                f=1;
-                break;
-            }
-
-
-        }
-
-        if(!f)
-        {
-            nums.push_back(newNums);
-        }
-        return nums;
-    
+         return res;
     }
 };

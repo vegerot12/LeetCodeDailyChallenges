2cb13c5a72cdc570bf0629855162dfd80e7dbaba
vegerot12 <nandhinirajavell@gmail.com>
2023-10-26 17:36:38 +0530

    Time: 29 ms (69.46%) | Memory: 18.9 MB (96.75%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -5,17 +5,33 @@ public:
         if(n <=1) return nums;
 
         sort(nums.begin(), nums.end());
-       vector<vector<int>> res;
-        res.push_back(nums[0]);
 
-        for(int i =1;i<n;i++){
-            if(max(res.back()[0] , nums[i][0]) <= min(res.back()[1] , nums[i][1]) ){
-                res.back()[0] = min(res.back()[0] , nums[i][0]);
-                 res.back()[1] = max(res.back()[1] , nums[i][1]);
 
-            }else 
-            res.push_back(nums[i]);
+    //    vector<vector<int>> res;
+    //     res.push_back(nums[0]);
+
+    //     for(int i =1;i<n;i++){
+    //         if(max(res.back()[0] , nums[i][0]) <= min(res.back()[1] , nums[i][1]) ){
+    //             res.back()[0] = min(res.back()[0] , nums[i][0]);
+    //              res.back()[1] = max(res.back()[1] , nums[i][1]);
+
+    //         }else 
+    //         res.push_back(nums[i]);
+    //     }
+    //     return res;
+
+   
+    int cur   =0;
+    for(int i =1;i<n;i++){
+        if(max(nums[cur][0], nums[i][0]) <= min(nums[cur][1], nums[i][1])){
+           nums[cur][0] = min(nums[cur][0], nums[i][0]);
+           nums[cur][1] = max(nums[cur][1], nums[i][1]);
+            
         }
-        return res;
+        else           
+        nums[++cur]=nums[i];
+    }
+     nums.erase(nums.begin()+cur+1, nums.end());
+     return nums;
     }
 };

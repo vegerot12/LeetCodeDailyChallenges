bff7eda6c1973a970d7f9e10125b73c86d255562
vegerot12 <nandhinirajavell@gmail.com>
2023-10-26 13:57:40 +0530

    Time: 17 ms (37.03%) | Memory: 17.4 MB (61.26%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
-    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInt) {
+        // using new array
+/*
          vector<vector<int>>  res;
          int n = intervals.size();
          
@@ -21,5 +24,48 @@ public:
              while(i<n)res.push_back(intervals[i++]);
        
          return res;
+         */
+
+// WITHOUT NEW ARR
+  int n = intervals.size();
+         
+         
+         bool added = false;
+
+         for(int i =0;i<n;i++){
+             // new interval ends before this starts
+             if(newInt[1]<intervals[i][0]){
+                 intervals.insert(intervals.begin() + i, newInt);
+                 added = true;
+                 break;
+             }
+                // overlap
+             else {
+                if(max(intervals[i][0], newInt[0]) <= min(intervals[i][1],newInt[1] )){
+                    int s = i, e=i;
+                    while(i<n && intervals[i][0] <= newInt[1] ){
+                        e = i;
+                        
+                        newInt[0] = min(intervals[i][0], newInt[0]);
+                        newInt[1] = max(intervals[i][1], newInt[1]);
+                        i++;
+
+
+                    }
+                   intervals.erase(intervals.begin() + s, intervals.begin() + e+1); // excliding end will be erased so +1 
+                   intervals.insert(intervals.begin() + s, newInt);
+                    added = true;
+                    break;
+                }
+             }
+         }
+
+         if(!added)
+         intervals.push_back(newInt);
+
+
+          
+return intervals;
+
     }
 };

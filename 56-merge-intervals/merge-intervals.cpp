ecb8b3736d33c5291980ee6a6436946ceb5b3f8e
vegerot12 <nandhinirajavell@gmail.com>
2023-10-26 17:14:42 +0530

    Time: 29 ms (69.46%) | Memory: 19.4 MB (60.72%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& nums) {
+        int n = nums.size();
+        if(n <=1) return nums;
+
+        sort(nums.begin(), nums.end());
+       vector<vector<int>> res;
+        res.push_back(nums[0]);
+
+        for(int i =1;i<n;i++){
+            if(max(res.back()[0] , nums[i][0]) <= min(res.back()[1] , nums[i][1]) ){
+                res.back()[0] = min(res.back()[0] , nums[i][0]);
+                 res.back()[1] = max(res.back()[1] , nums[i][1]);
+
+            }else 
+            res.push_back(nums[i]);
+        }
+        return res;
+    }
+};

4e0d920c2e8c686534521f35f8f6b7d401e797a3
vegerot12 <nandhinirajavell@gmail.com>
2023-10-29 22:36:24 +0530

    Time: 16 ms (41.16%) | Memory: 9.1 MB (34.59%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -3,38 +3,39 @@ public:
    
     vector<int> findAnagrams(string s, string p) {
 
-//         if(s.size() < p.size()) return {};
-//         unordered_map<int,int> m;
-//         vector<int> res;
+        if(s.size() < p.size()) return {};
+        unordered_map<char,int> m;
+        vector<int> res;
 
-//         for(int i =0;i<p.size() ;i++){
-//             m[p[i]-'a']++;
+        for(int i =0;i<p.size() ;i++){
+            m[p[i]]++;
             
-//         }
+        }
                 
 
-// int i = 0, match =0;
-// int r = 0;
+int i = 0, match =0;
+int r = 0;
 
-//         while( r<s.size()){
+       for(int r=0;r<s.size();r++){
             
-//             if(m.find(s[r]-'a') != m.end()){
-//               m[s[r] - 'a']--;
-//               cout<<s[r]<<m[s[r]-'a']<<endl;
-//               if(m[s[r]-'a'] == 0) match++;
-              
-//             }
-//             // cout<<match<<endl;
-//                 r++;
-//             if(match == m.size()) res.push_back(i);
-//             if(r >= p.size()-1 ){
-//               if(m.find(s[i]-'a') != m.end()){
-//               if(m[s[i]-'a'] == 0) match--;
-//               m[s[i] - 'a']++;
+            if(m.find(s[r]) != m.end()){
+              m[s[r] ]--;
 
-//             }
-//             i++;
-//             }
+              if(m[s[r]] == 0) match++;
+              
+            }
+           
+                
+            if(match == m.size()) res.push_back(i);
+           
+            if(r >= p.size()-1 ){
+              if(m.find(s[i]) != m.end()){
+              if(m[s[i]] == 0) match--;
+              m[s[i] ]++;
+
+            }
+            i++;
+            }
 
              
          
@@ -42,49 +43,49 @@ public:
 
             
             
-//         }
-//         return res;
+        }
+        return res;
    
 
-        vector<int> res;
-        int st = 0;
-        int matched = 0;
-        unordered_map<char, int> m;
+        // vector<int> res;
+        // int st = 0;
+        // int matched = 0;
+        // unordered_map<char, int> m;
 
-        for (int i = 0; i < p.size(); i++) {
-            m[p[i]]++;
-        }
+        // for (int i = 0; i < p.size(); i++) {
+        //     m[p[i]]++;
+        // }
 
-        for (int e = 0; e < s.size(); e++) {
+        // for (int e = 0; e < s.size(); e++) {
            
 
-            if (m.find(s[e]) != m.end()) {
-                m[s[e]]--;
+        //     if (m.find(s[e]) != m.end()) {
+        //         m[s[e]]--;
 
-                if (m[s[e]] == 0) {
-                    matched++;
-                }
-            }
+        //         if (m[s[e]] == 0) {
+        //             matched++;
+        //         }
+        //     }
 
-            if (matched == (int)m.size()) {
-                res.push_back(st);
-            }
+        //     if (matched == (int)m.size()) {
+        //         res.push_back(st);
+        //     }
 
-            if (e >= p.length() - 1) {
+        //     if (e >= p.length() - 1) {
                
 
-                if (m.find(s[st]) != m.end()) {
-                    if (m[s[st]] == 0) {
-                        matched--;
-                    }
+        //         if (m.find(s[st]) != m.end()) {
+        //             if (m[s[st]] == 0) {
+        //                 matched--;
+        //             }
 
-                    m[s[st]]++;
-                }
-                st++;
-            }
-        }
+        //             m[s[st]]++;
+        //         }
+        //         st++;
+        //     }
+        // }
 
-        return res;
+        // return res;
     }
 };
       

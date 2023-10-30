6d9f764b64d39787301475c68fef322816fd4b44
vegerot12 <nandhinirajavell@gmail.com>
2023-10-30 18:17:47 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (48.06%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -6,27 +6,16 @@ if(nums.size()==1) return nums[0];
         while(l<r){
 
             int m = (r-l)/2 + l;
-            //    if(nums[m]> nums[m+1]) return nums[m+1]; // if there are only 2 ele , m=l so m-1 will not exisit but m+1 wull exist so try m, m+1 before m,m-1
-            // if(nums[m] < nums[m-1]  ) return nums[m];
-            // if(nums[0] < nums[r]) return nums[0];
+               
+            if(nums[0] < nums[r]) return nums[0];
             
-            // if(nums[m] > nums[m+1]){ 
-            //     return nums[m+1];
-            // }
+            if(nums[m] > nums[m+1]) return nums[m+1];
+            if(nums[m] < nums[m-1]) return nums[m];
             
-            // if(nums[0] < nums[m]) l = m+1;
-            // else r = m-1;
-             if(nums[r] > nums[0]) return nums[0] ; // interval is sorted , unrotated so return first ele
-
-            if(nums[m]> nums[m+1]) return nums[m+1]; // if there are only 2 ele , m=l so m-1 will not exisit but m+1 wull exist so try m, m+1 before m,m-1
-            if(nums[m] < nums[m-1]  ) return nums[m];
-
-            if(nums[0] < nums[m]) {// left un rotated so right is rotated and has the smallest
-               l = m+1;
-            }
-            else // left is rotated 
-            r = m-1;
+            if(nums[0] < nums[m]) l = m+1;
+            else r = m-1;
         }
         return 0;
+        
     }
 };

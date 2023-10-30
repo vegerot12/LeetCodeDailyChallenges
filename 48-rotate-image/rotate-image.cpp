4f666713df7bf62b71f3a3bc089662255407992c
vegerot12 <nandhinirajavell@gmail.com>
2023-10-30 10:32:26 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (59.26%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -2,28 +2,15 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        int m = matrix[0].size();
 
-        vector<vector<int>> res;
-
-        for(int c=0;c<m;c++){
-            vector<int> tmp;
-            for(int r=0;r<n;r++){
-                tmp.insert(tmp.begin(),matrix[r][c] );
-                // tmp.push_back(matrix[r][c]);
+        for(int i =0;i<n;i++){
+            for(int j =0;j<i;j++){
+                swap(matrix[i][j], matrix[j][i]);
             }
-            res.push_back(tmp);
         }
 
-        // for(int i =0;i<n;i++){
-        //     for(int j =0;j<i;j++){
-        //         swap(matrix[i][j], matrix[j][i]);
-        //     }
-        // }
-
-        //  for(int i =0;i<n;i++){
-        //    reverse(matrix[i].begin(),matrix[i].end() );
-        // }
-        matrix = res;
+         for(int i =0;i<n;i++){
+           reverse(matrix[i].begin(),matrix[i].end() );
+        }
     }
 };

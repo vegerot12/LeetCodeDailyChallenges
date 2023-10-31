ced412f7463da5d0a604fc945d716564d74c4a77
vegerot12 <nandhinirajavell@gmail.com>
2023-10-31 13:49:26 +0530

    Time: 4 ms (64.22%) | Memory: 13.4 MB (72.30%) - LeetSync

---
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int days =0;
+        int n = grid.size(), m = grid[0].size();
+        queue<array<int,2>> q;
+        int offsets[5] = {0,1,0,-1,0}; // neightbour index
+
+        int rot = 0, tot =0;
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==2){ // if land then inc count
+                    
+                    grid[i][j] = 0; // mark as visited
+                    q.push({i,j}); // push in queue to check neighbors
+                    tot++;
+                }
+                if(grid[i][j]==1) tot++;
+                    
+            }
+           
+        }
+
+          while(!q.empty()){
+         int s = q.size();
+         while(s--){
+             auto p = q.front();
+                        q.pop();
+                        int x = p[0];
+                        int y = p[1];
+                        rot++;
+                       
+                        // check neighnor for land
+                        for(int k =0;k<4;k++){
+                            x= p[0]+offsets[k];
+                            y = p[1]+offsets[k+1];
+                            // if land found mark visited and push in queue
+                            if(x>=0 && x<n && y>=0 && y<m && grid[x][y]==1){
+                                 grid[x][y] =0;
+                                q.push({x,y});
+                            }
+                                
+                        }
+         }
+                        if(!q.empty()) days++;                       
+                    }
+       return rot == tot ? days : -1;
+    }
+};

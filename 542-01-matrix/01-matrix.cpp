1ff246bbae90a797ed7d76eabb0012fee4c25e84
vegerot12 <nandhinirajavell@gmail.com>
2023-10-31 12:30:15 +0530

    Time: 55 ms (89.58%) | Memory: 30.3 MB (88.64%) - LeetSync

---
+++ 542-01-matrix/01-matrix.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& matrix) {
+        int offset[5] = {0,1,0,-1,0};
+
+        int n = matrix.size(),m= matrix[0].size();
+
+        queue<pair<int,int>> q;
+        
+        for(int i =0;i<n;i++){
+            for(int j =0;j<m;j++){
+                if(matrix[i][j] == 0)
+                q.push({i,j});
+            }
+        }
+int dis = 0;
+vector<vector<int>> res = matrix;
+
+        while(!q.empty()){
+           int s = q.size();
+           while(s--){
+            auto p = q.front();
+            q.pop();
+
+            for(int k = 0 ; k< 4;k++){
+                int x = offset[k] + p.first;
+                int y = offset[k+1] + p.second;
+                if(x>=0 and x<n and y>=0 and y<m and matrix[x][y] == 1){
+                    res[x][y] = dis+1;
+                    matrix[x][y] = 0;
+
+                    q.push({x,y});
+
+                }
+            } 
+           }
+           dis++;
+
+
+
+        }
+        return res;
+    }
+};

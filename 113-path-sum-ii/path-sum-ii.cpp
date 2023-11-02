a7629a752a4f02a07b18dcb38e914a56261373a7
vegerot12 <nandhinirajavell@gmail.com>
2023-11-02 08:58:35 +0530

    Time: 6 ms (73.37%) | Memory: 20.2 MB (72.98%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.cpp
+++ 113-path-sum-ii/path-sum-ii.cpp
@@ -14,23 +14,17 @@ public:
     void findSum(TreeNode* root, vector<int>& cur, vector<vector<int>>& res,int& targetSum , int curSum){
 
         if(!root) return ;
-
-        // if(root->val + curSum > targetSum) return;
             
             curSum += root->val;
             cur.push_back(root->val);
-            cout<<curSum<< root->val<<endl;
+            
         if(!root->left and !root->right and targetSum==curSum ) {
             res.push_back(cur);
             
         }
-
-     
-
-      
-      
         findSum(root->left, cur,res,targetSum,curSum);
         findSum(root->right, cur,res,targetSum,curSum);
+       
         curSum -= root->val;
         cur.pop_back();
 

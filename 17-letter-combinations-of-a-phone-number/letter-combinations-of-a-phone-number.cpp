adcb35ba7f7076f0812824fb0e57d752b3c767ad
vegerot12 <nandhinirajavell@gmail.com>
2023-11-04 11:21:00 +0530

    Time: 0 ms (100.00%) | Memory: 6.9 MB (59.51%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    void dfs(string& digits,unordered_map <char, string>& m,  vector<string>& res, string& cur, int ind ){
+        if(ind == digits.size()) {
+            res.push_back(cur);
+            return ;
+        }
+
+       
+            for(char c: m[digits[ind]]){
+                cur.push_back(c);
+                dfs(digits,m,res,cur,ind+1);
+                cur.pop_back();
+            }
+        
+    }
+    vector<string> letterCombinations(string digits) {
+        unordered_map <char, string> m ;
+            m.insert({'2',"abc"});
+             m.insert({ '3', "def"});
+              m.insert({'4',"ghi"});
+               m.insert({'5',"jkl"});
+                m.insert( {'6',"mno"});
+                 m.insert({'7',"pqrs"});
+                 m.insert({'8',"tuv"});
+                  m.insert({'9',"wxyz"});
+                  
+
+        vector<string> res;
+        if(digits.size() == 0) return res;
+        string cur;
+        int ind  =0;
+
+        dfs(digits, m, res,cur,ind);
+        return res;
+        
+    }
+};

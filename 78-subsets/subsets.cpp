b0aca5f9a1a8c2e7b8830ae427c0b02ada6c40bb
vegerot12 <nandhinirajavell@gmail.com>
2023-11-04 10:50:30 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (83.17%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     void find(vector<int>& nums, int ind, vector<int>& cur,vector<vector<int>>& res){
         res.push_back(cur);
-        
+
         if(ind == nums.size()) {
         return;
 
@@ -15,10 +15,27 @@ public:
         }
     }
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> res;
-        vector<int> cur;
+        // vector<vector<int>> res();
+        // vector<int> cur;
 
-        find(nums, 0, cur, res);
+        // find(nums, 0, cur, res);
+
+        // ITERATIVE
+        int n = nums.size(), p= 1<<n; // 2^n
+        vector<vector<int>> res(p);
+
+        // based on the idea that the first ele is in every two subset
+        // second ele is in 2 of every four subset // third ele is in 4 out of every 8 subset 
+
+        for(int i = 0;i<p;i++){
+            for(int j=0;j<n;j++){ // to check if j th ele is in the subset 
+            if((i>>j) & 1) { // check if this subset number i , say if it is 7 and j is 2 and we move 7 two places left => 111 => 001 and 1 => 1 so this 2nd ele is in 7th subset so push it 
+                res[i].push_back(nums[j]);
+            }
+
+            }
+        }
+
         return res;
     }
 };

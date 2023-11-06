d048dc81bfb502ec6ffb396d72769ae3b540b9ce
vegerot12 <nandhinirajavell@gmail.com>
2023-11-06 19:03:03 +0530

    Time: 36 ms (5.39%) | Memory: 71.2 MB (95.65%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -12,23 +12,34 @@ class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         int carry = 0;
-        ListNode *dummyHeader =  new ListNode(0), *res = dummyHeader;
-        while(l1 || l2 || carry){
-            if(l1){
-                carry += l1->val;
-                l1 = l1->next;
-            }
-            if(l2){
-                carry += l2->val;
-                l2 = l2->next;
-            }
+        // ListNode *dummyHeader =  new ListNode(0), *res = dummyHeader;
+        ListNode* p2=l2;
+        while(l1-> next && l2->next ){
             
-                res-> next = new ListNode(carry%10);
+                carry += l1->val+l2->val;
+            
+                l2 -> val = carry%10;
                 carry = carry/10;
-            res = res->next;
+                l1 = l1->next;
+                l2 = l2->next;
+            
             
         }
+// if l2 is only there then no issues, we are anyways gonna return l2 directly 
+        while(carry || l1){
+            carry += (l1 ? l1->val : 0) + (l2 ? l2->val : 0);
+            l2 -> val = carry%10;
+                carry = carry/10;
 
-        return dummyHeader->next;
+         if (l1) l1 = l1 -> next;
+// in case if l2 is done and there is still carry or nodes in l1 then we create new ndoes in l2 , move l2 ptr to that and be ready for next iteration 
+         if(!l2 -> next && (carry || l1)){
+             l2 -> next  = new ListNode();
+         }
+         l2 = l2-> next;
+
+        }
+
+        return p2;
     }
 };

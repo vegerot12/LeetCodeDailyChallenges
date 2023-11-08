1043b6a1259b705b8503a33abfe48407211b002f
vegerot12 <nandhinirajavell@gmail.com>
2023-11-08 16:04:53 +0530

    Time: 48 ms (85.29%) | Memory: 34.7 MB (75.15%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-      unordered_map<char, int> f;
+   
       int maxF =0;
-      char charWithMaxF ;
-
+      int charWithMaxF ;
+ int f[26] = {0};
+     
       for(auto t:tasks){
-          f[t]++;
-          if(maxF < f[t]){
-          maxF =f[t];
-            charWithMaxF = t;
+          f[t-'A']++;
+          if(maxF < f[t-'A']){
+          maxF =f[t-'A'];
+            charWithMaxF = t-'A';
           }
       }
       maxF --;
       int idleSlots = (maxF) * n;
     
-      auto it = f.begin();
-      
-
-     while(it != f.end() and idleSlots>=0){
-         if(it->first == charWithMaxF) {
-             it++;
+    
+    
+        for(int i =0;i<26;i++){
+         if(i== charWithMaxF) {
+             
              continue;
          }
-          idleSlots -= min(maxF, it->second);
-        //   slotsUsed += it->second;
-            it++;
+          idleSlots -= min(maxF,f[i]);
+        
 
       }
-      cout<<tasks.size()<<idleSlots;
+     
 
       return idleSlots >0 ? idleSlots + tasks.size() : tasks.size() ; 
 

9a0e69647f40b898f4a366acf9b7b91a58764381
vegerot12 <nandhinirajavell@gmail.com>
2023-11-08 16:31:29 +0530

    Time: 11 ms (61.69%) | Memory: 13 MB (50.95%) - LeetSync

--- 692-top-k-frequent-words/top-k-frequent-words.cpp
+++ 692-top-k-frequent-words/top-k-frequent-words.cpp
@@ -12,19 +12,19 @@ public:
         vector<vector<string>> bucket(maxF+1);
 
         for(auto it: m){
-            cout<<"pushing"<<it.first<<"in"<<it.second<<"bucket"<<endl;
+          
             bucket[it.second].push_back(it.first);
         }
         vector<string> res;
         int f = maxF;
         while(true){
             auto& cur = bucket[f--];
-            cout<<cur.size();
+          
             sort(cur.begin(), cur.end());
 
             int i =0;
             while(i<cur.size()){
-                cout<<cur[i];
+               
                 res.push_back(cur[i++]);
                 if(res.size()==k) return res;
                 

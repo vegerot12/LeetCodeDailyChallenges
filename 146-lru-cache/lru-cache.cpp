b2572375a9d58049a0a5136a9dd6fd00a067923e
vegerot12 <nandhinirajavell@gmail.com>
2023-11-26 20:06:46 +0530

    Time: 419 ms (15.90%) | Memory: 183.2 MB (6.88%) - LeetSync

--- 146-lru-cache/lru-cache.cpp
+++ 146-lru-cache/lru-cache.cpp
@@ -1,80 +1,69 @@
 class Node{
     public:
-    Node* left = NULL;
-    Node* right = NULL;
-    int val;
     int key;
+    int val;
+    Node* prev=NULL,*next=NULL;
 
-    Node(int k, int x){
-        val = x;
+    Node(int k,int v){
         key = k;
+        val = v;
     }
-
-
-
 };
 class LRUCache {
-
+    unordered_map<int,Node*> m;
+    int cap;
+    Node* head=new Node(-1,-1), *tail=new Node(-1,-1);
 public:
-    unordered_map<int, Node*> m; // key, node
-    Node* head = new Node(0,0), *tail= new Node(0,0);
-    
-    int cap = 0;
-
-    void insertAtHead(int key, int val){
-        Node* nn = new Node(key, val); 
-        Node* tmp = head -> right;
-        head -> right = nn;
-        nn -> right = tmp;
-        nn -> left = head;
-        tmp -> left = nn;
-        m[key] = nn; 
-        
-    }
-
-    void remove(int key){
-        
-       Node* rn = m[key];
-       rn -> left -> right = rn -> right;
-       rn -> right -> left = rn -> left;
-       delete(rn);
-       m.erase(key);
-        
-    }
-
-    void insert(int key, int val){
-
-        if(m.find(key) == m.end()){
-            if(m.size() >= cap){
-                remove(tail->left->key);                
-            }
-            insertAtHead(key, val);
-
-        }
-        else{
-            remove(key);
-            insertAtHead(key, val);
-        }
-    }
-
-
     LRUCache(int capacity) {
         cap = capacity;
-        head -> right = tail;
-    tail -> left = head;
+        head -> next = tail;
+        tail -> prev = head;
+    }
+
+    void insertAtHead(int key,int value){
+        Node* tmp = head -> next;
+        Node* nn = new Node(key, value);
+        head -> next = nn;
+        nn -> prev = head;
+        nn -> next = tmp;
+        tmp -> prev = nn;
+        m[key] = nn;
+    }
+
+    void removeFromLast(){
+        Node* tmp = tail -> prev;
+        tmp-> prev -> next = tail;
+        tail -> prev = tmp -> prev;
+        m.erase(tmp->key);
+        delete(tmp);
+    }
+
+    int remove(int key){
+        Node *tmp = m[key];
+        tmp -> prev -> next = tmp -> next;
+        tmp -> next -> prev = tmp -> prev;
+        int val = m[key]->val;
+        m.erase(key);
+        delete(tmp);
+        return val;
     }
     
     int get(int key) {
         if(m.find(key) == m.end()) return -1;
-
-        int val = m[key]->val;
-        remove(key);
+        int val = remove(key);
         insertAtHead(key,val);
-        return val;
+        return m[key]->val;
     }
     
     void put(int key, int value) {
-        insert(key, value);
+        if(m.find(key) == m.end()){
+            if(m.size() == cap){
+                removeFromLast();
+            }
+        }else{
+            remove(key);        
+        }
+            insertAtHead(key, value);
     }
 };
 

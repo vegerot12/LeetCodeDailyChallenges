2b5c80bffbfd2fdab9a2ce9127ae20317d2d2a38
vegerot12 <nandhinirajavell@gmail.com>
2023-11-27 18:15:01 +0530

    Time: 7 ms (96.37%) | Memory: 17.5 MB (53.38%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& it, vector<int>& nit) {
         bool added = false;
-        int s=0,e=0, n = it.size();
+        int s=0, n = it.size();
 
         for(int i =0;i<n;i++){
             if(nit[1]< it[i][0]){
@@ -13,17 +13,17 @@ public:
             else{
                 if(max(it[i][0], nit[0]) <= min( it[i][1], nit[1])){
 
-                    s=i,e=i;
+                    // s=i,e=i;
                 while(i<n && max(it[i][0], nit[0]) <= min( it[i][1], nit[1])){
                    nit[0] = min(nit[0], it[i][0]);
                    nit[1] = max(nit[1], it[i][1]);
-                   e=i;
+                  
                    i++;
+                   s++;
 
                 }
-                cout<<s<<e;
- it.erase(it.begin()+s, it.begin()+e+1  );
-                it.insert(it.begin()+s, nit );
+ it.erase(it.begin()+i-s, it.begin()+i  );
+                it.insert(it.begin()+i-s, nit );
                 added = true;
                 break;
 

381c62b0c0b3b02bdb59a6026dde40cb436f4adb
vegerot12 <nandhinirajavell@gmail.com>
2023-11-28 12:22:36 +0530

    Time: 22 ms (97.87%) | Memory: 19 MB (96.82%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,21 +1,36 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& it) {
-        sort(it.begin(), it.end());
-        int cur = 0;
+    vector<vector<int>> merge(vector<vector<int>>& nums) {
+        int n = nums.size();
+        if(n <=1) return nums;
 
-        for(int i =1;i<it.size();i++){
-            if(max(it[i][0] , it[cur][0]) <= min(it[i][1], it[cur][1])){
-                it[cur][0]= min(it[i][0] , it[cur][0]);
-                it[cur][1]= max(it[i][1] , it[cur][1]);
+        sort(nums.begin(), nums.end());
 
-            }
-            else{
-                it[++cur] = it[i];
-            }
+
+    //    vector<vector<int>> res;
+    //     res.push_back(nums[0]);
+
+    //     for(int i =1;i<n;i++){
+    //         if(max(res.back()[0] , nums[i][0]) <= min(res.back()[1] , nums[i][1]) ){
+    //             res.back()[0] = min(res.back()[0] , nums[i][0]);
+    //              res.back()[1] = max(res.back()[1] , nums[i][1]);
+
+    //         }else 
+    //         res.push_back(nums[i]);
+    //     }
+    //     return res;
+
+   
+    int cur   =0;
+    for(int i =1;i<n;i++){
+        if(max(nums[cur][0], nums[i][0]) <= min(nums[cur][1], nums[i][1])){
+           nums[cur][0] = min(nums[cur][0], nums[i][0]);
+           nums[cur][1] = max(nums[cur][1], nums[i][1]);   
         }
-
-        it.erase(it.begin() + cur+1, it.end());
-        return it;
+        else           
+        nums[++cur]=nums[i];
+    }
+     nums.erase(nums.begin()+cur+1, nums.end());
+     return nums;
     }
 };

d6b550a52342a2ce56e0ffb2d79665ed4196d4ba
vegerot12 <nandhinirajavell@gmail.com>
2023-12-01 17:17:19 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (98.61%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if(nums.size()==1) return nums[0];
+       if(nums.size()==2) return max(nums[0], nums[1]);
+        if( nums.size() <3) return max( (nums[0]+nums[2]) , nums[1]) ;
+        int f1 =0, s1 =0, f2=0, s2=0, r1=0,r2=0;
+        f1 = nums[0], s1 = max(nums[0], nums[1]), s2 = max(nums[1], nums[2]), f2 = nums[1];
+        r1 = max(f1,s1); 
+        r2 = max(f2,s2);
+        for(int i=2;i<nums.size();i++){
+            if(i!= nums.size()-1 ){
+            r1 = max(f1 +   nums[i] , s1);
+                 f1 = s1;
+            s1 = r1;
+            }
+            if(i!=2 ){
+            r2 = max(f2 + nums[i] , s2);
+
+             f2 = s2;
+            s2 = r2;
+            }
+cout<<i<<f1<<f2<<s1<<s2<<r1<<r2<<endl;
+           
+        }
+        return max(r1,r2);
+    }
+};

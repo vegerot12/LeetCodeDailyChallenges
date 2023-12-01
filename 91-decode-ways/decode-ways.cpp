300ef0507d59584c435dad7ab35ca655196421a1
vegerot12 <nandhinirajavell@gmail.com>
2023-12-01 19:23:36 +0530

    Time: 0 ms (100.00%) | Memory: 6.6 MB (65.08%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
-
-   
-
     int numDecodings(string s) {
-         int n = s.size();
-         if(s[0] == '0') return 0;
-           if(s.size()<=1) return 1;
+        int n = s.size();
 
-         vector<int> dp(n+1, 0);
-         dp[0] = dp[1] = 1; // 1 ways fr 0 and 1 num of strings
+        if(s[0] == '0' ) return 0;
+        if(n==1) return 1;
+        vector<int> res(n+1,0);
+
+        res[0] = res[1] = 1;
+
+
 
         for(int i = 2;i<=n;i++){
-         int oneDigit = s[i-1]-'0';
-         int twoDigit = (s[i-2]-'0')*10 + (s[i-1]-'0');
+            int oneDigit = s[i-1]-'0';
+            int twoDigit = (s[i-2]-'0')*10 +( s[i-1]-'0');
 
-         if(oneDigit >0) dp[i] += dp[i-1];
-         if(twoDigit >=10 and twoDigit <= 26) dp[i] += dp[i-2];
+            if(oneDigit > 0) 
+            res[i] += res[i-1];
 
-
-
+            if(twoDigit >= 10 and twoDigit <= 26) res[i] += res[i-2];
         }
-return dp[n];
+        return res[n];
     }
-
-
 };

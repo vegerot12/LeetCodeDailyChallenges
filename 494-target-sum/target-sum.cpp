5b686c2b0b9cce4b91840ca1198ae744c274a042
vegerot12 <nandhinirajavell@gmail.com>
2023-12-02 18:37:02 +0530

    Time: 25 ms (56.75%) | Memory: 22.3 MB (32.06%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -16,7 +16,7 @@ int findSum(vector<int>& nums, int target,int sum,  vector<vector<int>> &dp, int
 
 
 }
-    int findTargetSumWays(vector<int>& nums, int target) {
+    int findTargetSumWaysMemo(vector<int>& nums, int target) {
         int sum = 0;
         for(int i =0;i<nums.size();i++){
             sum+= nums[i];
@@ -25,4 +25,31 @@ int findSum(vector<int>& nums, int target,int sum,  vector<vector<int>> &dp, int
 
         return findSum(nums, target,sum, dp, 0,0);
     }
+
+      int findTargetSumWays(vector<int>& nums, int target) {
+int tot = 0;
+        for(int i =0;i<nums.size();i++){
+            tot+= nums[i];
+        }
+
+        if(abs(target) > tot) return 0;
+
+        vector<vector<int>> dp(nums.size(), vector<int>( 2*tot+1, 0));
+
+        dp[0][tot + nums[0]] += 1;
+        dp[0][tot - nums[0]]  += 1;
+
+        for(int i = 1; i<nums.size();i++){
+            
+            for(int j = -tot;j<=tot ; j++){
+                if(dp[i-1][j+tot] > 0){
+                    dp[i][tot + j+ nums[i]] += dp[i-1][tot+j];
+                    dp[i][tot + j- nums[i]] += dp[i-1][tot+j];
+
+                }
+            }
+        }
+
+return dp[nums.size()-1][target+tot] ;
+      }
 };

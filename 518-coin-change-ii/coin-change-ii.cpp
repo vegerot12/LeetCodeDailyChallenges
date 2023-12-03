3cbbabb44cbd30b720c8010c4cd3adf7569c94ce
vegerot12 <nandhinirajavell@gmail.com>
2023-12-03 09:54:38 +0530

    Time: 17 ms (57.39%) | Memory: 7.6 MB (83.22%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int findSum( vector<int>& coins, int ind, int amount, vector<vector<int>>& memo ){
+        if(ind == coins.size()) {
+            if(amount == 0) return 1;
+            return 0;
+        }
+
+        if(memo[ind][amount]!=0) return memo[ind][amount]; 
+
+        int notTaken = findSum(coins, ind+1, amount, memo);
+        int taken = 0;
+
+        if(amount >= coins[ind])
+        taken = findSum(coins, ind, amount-coins[ind], memo);
+
+        return memo[ind][amount]= taken + notTaken;
+    }
+    int changeMemo(int amount, vector<int>& coins) {
+        int n = coins.size();
+
+         vector<vector<int>> memo(n, vector<int> (amount+1,0));
+
+        return findSum(coins, 0,amount,memo);
+
+    }
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+
+     vector<int> dp(amount+1,0);
+
+     dp[0] =1 ;
+
+     for(int i =0;i<n;i++){
+         for(int j = 0;j<=amount;j++){
+             if(j>=coins[i]){
+                 dp[j]  +=  dp[j-coins[i]];
+             }
+         }
+     }
+
+        return dp[amount]  ;
+
+    }
+};

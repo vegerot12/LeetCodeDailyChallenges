c296ddb4e66a553360a72fb232051e0bef968cb1
vegerot12 <nandhinirajavell@gmail.com>
2023-12-20 17:17:42 +0530

    Time: 9 ms (33.53%) | Memory: 9.3 MB (46.64%) - LeetSync

--- 684-redundant-connection/redundant-connection.cpp
+++ 684-redundant-connection/redundant-connection.cpp
@@ -1,51 +1,35 @@
-class DSU{
+  class Solution {
+    private:
+    int find(int p,vector<int>& parent){
 
-    vector<int> parent, size;
-
-    public:
-    DSU(int n){
-        parent.resize(n,-1);
-        size.resize(n,1);
+        if(parent[p] == p) return p;
+        return parent[p] = find(parent[p],parent);
+    //   if(parent[p]!=p)
+    //   parent[p] = find(parent[p],parent);
+    //   return parent[p];
     }
 
-
-    int findParent(int x){
-        if(parent[x] == -1)  return x ;
-        return parent[x] = findParent(parent[x]);
+    int unionEdge(int a, int b, vector<int>& parent){
+        int p1 = find(a,parent);
+        int p2 = find(b, parent);
+        if(p1==p2) return -1; // if it already has same parent return -1 meaning this node has to be removed
+        return parent[p1]=p2; // if they both have diff parent since there exisits a edge between them now make it as same parent 
     }
-
-    bool unionParent(int x, int y){
-        x = findParent(x);
-        y = findParent(y);
-
-        if( x == y) return false;
-
-        if(size[x] > size[y]){
-        parent[y] = x;
-        size[x] += size[y];
-        }
-        else {
-            parent[x] = y;
-            size[y] += size[x];
-        }
-return true;
-    }
-    
-};
-
-
-class Solution {
 public:
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-         int n =edges.size();
-        DSU dsu(n+1);
-        vector<int> res;
-        
-        for(auto e:edges){
-            if(!dsu.unionParent(e[0], e[1]))
-            res = e;
+        int n = edges.size(); // graph has been started as a tree(tree of n nodes has n-1 edges) and one edge got added extra in the graph - so n-1+1 = the graph has n edges , and n nodes
 
-        }
-        return res;
+        vector<int> parent(n+1); // since nodes from 1 to n
+       // initialise parent as itself
+       for(int i=1;i<=n;i++) 
+       parent[i] =i;
+
+       // for each of n edges chek if any edge has same parent
+       for(int i=0;i<n;i++){
+           int x = unionEdge(edges[i][0], edges[i][1], parent); // keep merging nodes connected by each of n edges 
+           // when we come across a nodes that already are connected return that edge as it is not needed
+           if(x==-1) return edges[i];
+       }
+       return edges[n-1];
     }
 };

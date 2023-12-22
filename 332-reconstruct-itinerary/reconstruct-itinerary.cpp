68e53b42c8c7bd52b67b5e0931ebda856d4f7736
vegerot12 <nandhinirajavell@gmail.com>
2023-12-22 22:38:31 +0530

    Time: 25 ms (19.92%) | Memory: 14.8 MB (66.54%) - LeetSync

---
+++ 332-reconstruct-itinerary/reconstruct-itinerary.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    private:
+          unordered_map<string,priority_queue<string, vector<string>, greater<>>> adj;
+          vector<string> res;
+          void dfs(string src){
+             while( adj.find(src) != adj.end() && !adj[src].empty()){
+                 auto node = adj[src].top();
+                 adj[src].pop();
+                 dfs(node);
+             }
+             res.push_back(src);
+          }
+public:
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+          sort(tickets.begin(), tickets.end());
+          for(auto& t:tickets){
+              adj[t[0]].push(t[1]); // tickets are diretced edges
+          }
+          int n = tickets.size();
+          dfs("JFK" );
+          reverse(res.begin(), res.end());
+          return res;
+    }
+};

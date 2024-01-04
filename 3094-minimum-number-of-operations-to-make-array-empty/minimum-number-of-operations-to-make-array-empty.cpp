94bfdb8be0ea36e03fdbcea197a9a8493b57dc41
vegerot12 <nandhinirajavell@gmail.com>
2024-01-04 19:06:28 +0530

    Time: 116 ms (74.59%) | Memory: 85.2 MB (63.17%) - LeetSync

--- 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.cpp
+++ 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.cpp
@@ -1,3 +1,22 @@
+/*
+
+Count	Operations required to remove elements
+1	return -1
+2	1
+3	1
+4	2
+5	2
+6	2
+7	3
+8	3
+9	3
+10	4
+11	4
+12	4
+From the information presented in this table, we can deduce that the number of operations needed to remove a total of count elements of a given kind is represented by the expression ceil(count / 3), where the ceil method rounds up the decimal result of count / 3. Except in the scenario where the count of the element is 1, making it impossible to remove elements of that kind, in which case we should return -1.
+
+O(N) and O(n)
+*/
 class Solution {
 public:
     int minOperations(vector<int>& nums) {

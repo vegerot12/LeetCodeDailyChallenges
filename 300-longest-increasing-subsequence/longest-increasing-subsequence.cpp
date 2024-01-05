d2eaba73decfa16c248ff1d01589208ac3fceb88
vegerot12 <nandhinirajavell@gmail.com>
2024-01-05 17:41:03 +0530

    Time: 4 ms (93.93%) | Memory: 10.6 MB (99.33%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
+        if(nums.size() == 0) return 0;
         vector<int> lis;
-        int n = nums.size();
 
         lis.push_back(nums[0]);
 
-        for(int i =1 ;i<n;i++){
+        for(int i =1; i< nums.size();i++){
 
-            if(lis.back() < nums[i]){
-                lis.push_back(nums[i]);
-            }
+            if(lis.back() < nums[i])
+            lis.push_back(nums[i]);
             else{
-                int posOfNextGrter = lower_bound(lis.begin(), lis.end(), nums[i])-lis.begin();
-                lis[posOfNextGrter] = nums[i];
+                auto pos = lower_bound(lis.begin(), lis.end(), nums[i])- lis.begin();
+                lis[pos] = nums[i];
+
             }
-
-
         }
         return lis.size();
     }

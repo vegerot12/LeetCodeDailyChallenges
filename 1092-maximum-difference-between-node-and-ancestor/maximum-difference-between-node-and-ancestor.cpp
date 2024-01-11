3360aae923a73ca994ebbc0f561db9a632d427f7
vegerot12 <nandhinirajavell@gmail.com>
2024-01-11 18:40:27 +0530

    Time: 3 ms (85.41%) | Memory: 10.1 MB (79.85%) - LeetSync

---
+++ 1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxAncestorDiff(TreeNode* root, int mn = 10000, int mx=0) {
+
+        if(!root) return mx-mn;
+
+        mx = max(mx, root->val);
+        mn = min(mn, root->val);
+
+        return max(maxAncestorDiff( root->left, mn, mx), maxAncestorDiff(root->right, mn, mx));
+
+        
+    }
+};

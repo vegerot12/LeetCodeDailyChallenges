808a9fe690659f94dcafae2d40cb563048fb5da8
vegerot12 <nandhinirajavell@gmail.com>
2024-01-22 12:51:03 +0530

    Time: 21 ms (80.53%) | Memory: 23.8 MB (36.81%) - LeetSync

--- 645-set-mismatch/set-mismatch.cpp
+++ 645-set-mismatch/set-mismatch.cpp
@@ -11,17 +11,17 @@ int n = nums.size();
         for(int i =0;i<n;i++){
             // num at index = cur num 
             int cur = abs(nums[i]);
+           // if any num is already -ve, we negate  we add itt to duplocate 
+           if(nums[cur-1]< 0) res[0] = cur; // dup
             // we visit the index = cur num, so we change the number at prev index , because this cur num say = 1 is supposed to be at 0th index 
-           nums[cur-1] = nums[cur-1] * -1;
+           else nums[cur-1] = nums[cur-1] * -1;
 
-           // if any num is twice, we negate the same index twice so its positive so we add itt to duplocate 
-           if(nums[cur-1]> 0) res[0] = cur; // dup
             
         }
 
-// now we look for all nums if there is a index thats posititve then that index has not been visited. so if index = 0 then number supposed to be at tat index is index+1 so we take i+1 as the missing number provided this i+1 is not the duplicate number because an index can also have pos number if its visited twice 
+// now we look for all nums if there is a index thats posititve then that index has not been visited. so if index = 0 then number supposed to be at tat index is index+1 so we take i+1 as the missing number 
         for(int i =0;i<n;i++){
-            if(nums[i] > 0 and res[0] != i+1) {
+            if(nums[i] > 0 ) {
 
             res[1] = i+1;
             return res;

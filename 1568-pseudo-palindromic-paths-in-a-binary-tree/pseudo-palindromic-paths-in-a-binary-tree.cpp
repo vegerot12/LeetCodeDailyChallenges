4c14efdaecc6e34b9e9f940277b0a897287fa2c0
vegerot12 <nandhinirajavell@gmail.com>
2024-01-24 12:52:18 +0530

    Time: 267 ms (65.95%) | Memory: 179.8 MB (73.19%) - LeetSync

--- 1568-pseudo-palindromic-paths-in-a-binary-tree/pseudo-palindromic-paths-in-a-binary-tree.cpp
+++ 1568-pseudo-palindromic-paths-in-a-binary-tree/pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -15,9 +15,9 @@
 - if len == 1 
 - if the cnt of each char is even except for atmost  1 
 - find all root to leaf paths and check for pal 
+// O(n) and O(n)
 
 
- */
 class Solution {
 public:
     bool isPal(unordered_map<int, int>& m){
@@ -53,6 +53,44 @@ public:
         unordered_map<int, int> m;
         int res =0;
         findPath(root, res, path , m);
+        return res;
+    }
+};
+ */
+
+
+ // OPTIMISATION 
+
+class Solution {
+public:
+   
+
+    void findPath(TreeNode* root, int& res, unordered_map<int, int>& m, int& odd ){
+        if(!root) return;
+
+        
+        m[root->val] ++;
+        int odd_change =  m[root->val] % 2 ? 1 : -1;
+        odd += odd_change; // we increase or decrease odd based on cur num 
+
+
+        // leaf
+        if(!root-> left && !root->right){
+            if(odd<=1) res++;
+        } 
+
+        findPath(root->left, res,m, odd);
+        findPath(root->right, res, m, odd);
+
+        odd -= odd_change; // if we increased before while removing that num we decrease and vice versa 
+        m[root->val]--;
+
+    }
+    int pseudoPalindromicPaths (TreeNode* root) {
+       
+        unordered_map<int, int> m;
+        int res =0, odd=0;
+        findPath(root, res, m, odd);
         return res;
     }
 };

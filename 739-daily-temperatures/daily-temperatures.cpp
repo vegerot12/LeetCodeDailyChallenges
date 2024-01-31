0b9029716e95c36cc3f56031b046a0531020a667
vegerot12 <nandhinirajavell@gmail.com>
2024-01-31 17:21:33 +0530

    Time: 92 ms (99.60%) | Memory: 100.8 MB (69.22%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.cpp
+++ 739-daily-temperatures/daily-temperatures.cpp
@@ -1,34 +1,26 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& t) {
-
-
         int n = t.size();
-        vector<int> res(n,0);
         int hottest = t[n-1];
-        
+        vector<int> res(n, 0);
 
-        for(int i = n-2; i>=0;i--){
-
+        for(int i =n-2 ; i>= 0 ;i--){
             if(t[i] >= hottest) {
                 hottest = t[i];
                 continue;
             }
 
+            int days = 1;
 
-            
-            
-                int days = 1;
-                while( t[i] >= t[i+days]){
-                    
-                    days += res[i+days];
-                }
-              
-                 res[i] = days;
-            
+            while(i+days < n and t[i] >= t[i+days]){
+                days= days+ res[days+i];
+
+            }
+
+            res[i] = days;
+
         }
-
-
         return res;
     }
 };

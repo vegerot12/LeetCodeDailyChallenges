8ae51cfc9e1ece24067d85dfdcb76df6788c2656
vegerot12 <nandhinirajavell@gmail.com>
2024-02-07 22:53:53 +0530

    Time: 98 ms (65.01%) | Memory: 11 MB (27.48%) - LeetSync

---
+++ 279-perfect-squares/perfect-squares.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int numSquares(int n) {
+
+
+        int sqrCnt = (int)sqrt(n)+1;
+        vector<int> sqr; 
+        // /(sqrCnt)
+
+        // n is the total amt we need to make
+        vector<int> dp(n+1, n+1);
+        dp[0] = 0;
+
+// these are our coins 
+        for(int i =0;i<sqrCnt;i++){ 
+            sqr.push_back(i*i);
+        }
+
+        // amt
+        for(int i = 1;i<=n;i++){
+            // coins
+            for(int j = 1;j<sqrCnt;j++){
+                if(i>=sqr[j]){
+                    dp[i] = min(dp[i], dp[i-sqr[j]]+1);
+                }
+            }
+        }
+        return dp[n];
+    }
+};

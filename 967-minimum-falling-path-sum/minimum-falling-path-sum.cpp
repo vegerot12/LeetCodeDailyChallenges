15c7bda17e99538341ca014caf1f8657292ba7c4
vegerot12 <nandhinirajavell@gmail.com>
2024-02-11 01:27:28 +0530

    Time: 5 ms (96.69%) | Memory: 12.9 MB (38.22%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -4,6 +4,7 @@ public:
          int n = matrix.size(), m = matrix[0].size();
 
     //  Iterate over matrix
+
     for (int row = 1; row < n; row++)
     {
         for (int col = 0; col < m; col++)
@@ -22,6 +23,40 @@ public:
     {
        
             maxPathSum = min(maxPathSum, matrix[n - 1][col]);
+        
+    }
+    return maxPathSum;
+    }
+
+    int minFallingPathSum1Darr(vector<vector<int>>& matrix) {
+         int n = matrix.size(), m = matrix[0].size();
+
+         vector<int> prev(m,INT_MAX);
+         prev = matrix[0];
+
+    //  Iterate over matrix
+    for (int row = 1; row < n; row++)
+    {
+    vector<int> dp(m,INT_MAX);
+
+
+        for (int col = 0; col < m; col++)
+        {
+            //  Get maximum of all valid path
+            int top = prev[col];
+            int topLeft =  prev[max(0, col - 1)];
+            int topRight =prev[min(m - 1, col + 1)];
+            dp[col] = matrix[row][col] + min({top,topLeft , topRight});
+        }
+        prev = dp;
+    }
+
+    //  Get the max path sum
+    int maxPathSum = INT_MAX;
+    for (int col = 0; col < m; col++)
+    {
+       
+            maxPathSum = min(maxPathSum, prev[col]);
         
     }
     return maxPathSum;

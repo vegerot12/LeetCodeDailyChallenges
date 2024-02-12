cab93204f494f86e536ef364798e6912abb0e6ed
vegerot12 <nandhinirajavell@gmail.com>
2024-02-12 12:01:14 +0530

    Time: 15 ms (97.67%) | Memory: 16.2 MB (48.59%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,38 +1,22 @@
-// class Solution {
-// public:
-//     int coinChange(vector<int>& coins, int amount) {
-       
-//         int n = coins.size();
-        
-
-//         vector<int> dp(amount+1, amount+1);
-
-//         dp[0] = 0;
-
-//         for(int i =0;i<n;i++){
-//             for(int t = 1;t<=amount ; t++){
-//                 if(t>=coins[i])
-//                 dp[t] = min(dp[t], dp[t-coins[i]]) +1;
-//             }
-//         }
-
-//         return dp[amount] < amount+1 ? dp[amount]:-1 ;
-//     }
-// };
-
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
+       
+        int n = coins.size();
         
-        vector<int> dp(amount+1, amount+1);
-        dp[0]=0; 
 
-        for(int i=0;i<coins.size();i++){
-            for(int j =1;j<=amount;j++){
-                if(j>=coins[i])
-                dp[j] = min(dp[j], dp[j-coins[i]]+1);
+        vector<int> dp(amount+1, amount+1);
+
+        dp[0] = 0;
+
+        for(int i =0;i<n;i++){
+            for(int t = 1;t<=amount ; t++){
+                if(t>=coins[i])
+                dp[t] = min(dp[t], dp[t-coins[i]]+1);
             }
         }
-        return dp[amount] == amount+1 ? -1 : dp[amount];
+
+        return dp[amount] < amount+1 ? dp[amount]:-1 ;
     }
 };
+

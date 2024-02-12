e16a44559e7323af416c94ec0dcbacfd40f9cfb7
vegerot12 <nandhinirajavell@gmail.com>
2024-02-12 12:00:00 +0530

    Time: 20 ms (93.00%) | Memory: 16.2 MB (48.59%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,62 +1,38 @@
+// class Solution {
+// public:
+//     int coinChange(vector<int>& coins, int amount) {
+       
+//         int n = coins.size();
+        
+
+//         vector<int> dp(amount+1, amount+1);
+
+//         dp[0] = 0;
+
+//         for(int i =0;i<n;i++){
+//             for(int t = 1;t<=amount ; t++){
+//                 if(t>=coins[i])
+//                 dp[t] = min(dp[t], dp[t-coins[i]]) +1;
+//             }
+//         }
+
+//         return dp[amount] < amount+1 ? dp[amount]:-1 ;
+//     }
+// };
+
 class Solution {
 public:
-    int findCoin(vector<int>& coins, int amount,
-    // int cur, 
-    int ind,
-     vector<vector<int>>& dp
-    ){
-        if(ind >= coins.size() || amount <= 0){
-           return amount == 0 ? 0:INT_MAX-1;
-            
-        }
-        if(dp[ind][amount] != -1) return dp[ind][amount];
-        
-        int notTaken = findCoin(coins,amount ,ind+1, dp);
-        int taken = INT_MAX;
-
-        if(amount >= coins[ind] ){
-            taken = 1 + findCoin(coins, amount - coins[ind], ind, dp);
-        }
-
-        return dp[ind][amount] =  min(taken, notTaken);
-
-        
-        
-    }
-    int coinChangeMemoisation(vector<int>& coins, int amount) {
-        
-        vector<vector<int>> dp(coins.size(), vector<int> (amount+1, -1));
-        
-       int res = findCoin(coins,amount, 0,dp);
-        return res == INT_MAX - 1 ? -1 : res;
-    }
-
     int coinChange(vector<int>& coins, int amount) {
         
-        vector<int> dp( amount+1, amount+1);
+        vector<int> dp(amount+1, amount+1);
+        dp[0]=0; 
 
-       
-        dp[0] = 0;
-        
-       for(int i = 1;i<=coins.size() ;i++){
-           for(int j =1;j<=amount;j++){
-               if(j >= coins[i-1])
-               dp[j]= min(1+dp[j-coins[i-1]] , dp[j]) ;
-           }
-       }
-       return dp[amount] > amount ? -1 : dp[amount];
-
-    //    vector<int> dp(amount+1, amount+1);
-    //     dp[0] =0;
-    //   for(int i=1;i<=coins.size();i++){
-    //       for(int j=1;j<=amount;j++){
-    //           if(j>=coins[i-1]){
-    //               dp[j] = min(dp[j], dp[j-coins[i-1]]+1);
-    //           }
-    //       }
-    //   }
-    //   return dp[amount] > amount ? -1 : dp[amount];
+        for(int i=0;i<coins.size();i++){
+            for(int j =1;j<=amount;j++){
+                if(j>=coins[i])
+                dp[j] = min(dp[j], dp[j-coins[i]]+1);
+            }
+        }
+        return dp[amount] == amount+1 ? -1 : dp[amount];
     }
-
-    
 };

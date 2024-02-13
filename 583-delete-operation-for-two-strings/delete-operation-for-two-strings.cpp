98a267a8fadbf5e4feb45b14b856f52f7e2e3f92
vegerot12 <nandhinirajavell@gmail.com>
2024-02-13 13:26:47 +0530

    Time: 10 ms (80.89%) | Memory: 10.1 MB (82.22%) - LeetSync

---
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int longestCommonSubstring(string &s, string &t){
+        int n = s.size(), m = t.size();
+
+        vector<int> dp(m+1,0) , cur(m+1,0);
+        int res  =0;
+
+        for(int i =1;i<=n;i++){
+            for(int j =1;j<=m;j++){
+                if(s[i-1] == t[j-1]){
+                    cur[j] = 1 + dp[j-1];
+                    // res = max(res, cur[j]);
+                }
+                else cur[j] = max(dp[j], cur[j-1]);
+            }
+            dp = cur;
+        }
+        return dp[m];
+    }
+    int minDistance(string word1, string word2) {
+        int tot = word1.size() + word2.size();
+        return tot - 2*(longestCommonSubstring(word1, word2));
+     }
+};

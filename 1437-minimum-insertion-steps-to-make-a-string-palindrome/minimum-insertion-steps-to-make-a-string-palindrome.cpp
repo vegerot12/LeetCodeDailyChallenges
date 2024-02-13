4e68e8e4d25e28fca7a60d9e1e38c606073fb3c8
vegerot12 <nandhinirajavell@gmail.com>
2024-02-13 12:43:13 +0530

    Time: 27 ms (78.26%) | Memory: 7.9 MB (86.00%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -13,12 +13,27 @@ class Solution {
         return dp[n][n];
 
     }
+    int lcsSpaceOptim(string& s, string &t, int &n){
+        vector<int> dp(n+1, 0), cur(n+1,0);
+
+        for(int i = 1;i <=n; i++){
+             for(int j=1; j<=n ;j++){
+                 if(s[i-1] == t[j-1]) 
+                 cur[j] = 1 + dp[j-1]; // j-1 alrd updated in cur row 
+                 else
+                 cur[j] = max(dp[j], cur[j-1]); // j-1 updated so its same as i,j-1 and j not updated so its i-1,j
+             }
+             dp=cur;
+        }
+        return dp[n];
+
+    }
 public:
     int minInsertions(string s) {
         int n = s.size();
         string t = s;
         reverse(t.begin(), t.end());
 
-        return n - lcs(s,t,n);
+        return n - lcsSpaceOptim(s,t,n);
     }
 };

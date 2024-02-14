5f2cf6b67bc8fba6fadbd10905441c86a1c7a815
vegerot12 <nandhinirajavell@gmail.com>
2024-02-14 10:33:27 +0530

    Time: 0 ms (100.00%) | Memory: 19.7 MB (49.52%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,35 +1,23 @@
 class Solution {
 public:
-int dfs(string& s, string& t, int i, int j, int n,int m,  map<pair<int,int>, int>& cache){
-    if(j==m ) return 1;
-    if(i ==n) return 0;
 
-    if(cache.find({i,j})!= cache.end()){
-        return cache[{i,j}];
-    }
-    // if(s[i]==t[j])
-    // cache[{i,j}] = dfs(s,t,i+1,j+1,n,m,cache) + dfs(s,t,i+1,j,n,m,cache);
-    // else
-    // cache[{i,j}] = dfs(s,t,i+1,j,n,m,cache);
-      if(s[i]==t[j])
-    cache[{i,j}] = dfs(s,t,i+1,j+1,n,m,cache) ;
-    
-    cache[{i,j}] += dfs(s,t,i+1,j,n,m,cache);
+    int findMatch(string &s, string &t, int i, int j, vector<vector<int>> &dp ){
+        if(j == 0) return dp[i][j] = 1;
+         if(i<j || i==0) return dp[i][j] =  0;
 
-    return cache[{i,j}];
-
-
-}
-// how many subsequences of s equals to t 
-    int numDistinct(string s, string t) {
-        int n = s.size() , m = t.size();
-        if(m==0) return 1; // if t is empty "" subsequence of s  matches full of t which is ""
-        if(n==0) return 0; // if s is empty only one subsequence "" exists in s and we cant match whole of t with that so 0
-
-        map<pair<int,int>, int> cache;
-
-       return dfs(s,t, 0, 0, n,m,cache);
+        if(dp[i][j]>=0) return dp[i][j];
 
         
+        dp[i][j] = findMatch(s,t,i-1,j,dp) ; // move str alone
+        if(s[i-1] == t[j-1])
+        dp[i][j] =  dp[i][j] + findMatch(s,t,i-1,j-1,dp); // move str alone + move both str and pat
+
+        return dp[i][j];
+    }
+    int numDistinct(string s, string t) {
+        int n = s.size() , m = t.size();
+        vector<vector<int>> dp(n+1, vector<int>(m+1,-1));
+
+        return findMatch(s,t,n,m,dp);
     }
 };

e4c430f411e295dd378df7fae7554ad8c0c68cd3
vegerot12 <nandhinirajavell@gmail.com>
2024-02-18 18:49:34 +0530

    Time: 3 ms (96.46%) | Memory: 10.2 MB (59.60%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,17 +1,23 @@
 class Solution {
-    //     vector<int> m(256,-1); // stores last seen index of all chars 
-//         int start = -1, ml =0;
-        
-//         for(int i=0;i<s.size();i++){
-//             if(m[s[i]]>start)
-//                 start=m[s[i]];
-//             m[s[i]]=i;
-//             ml=max(ml,i-start);
-//         }
-//     return ml;
-    // O(N) and O(N)
 public:
+    
+
+    // O(N) and O(N)
     int lengthOfLongestSubstring(string s) {
+        vector<int> v(256,-1); // initially lastSeenindex of all is -1 -> store char, lastSeenindex
+        int len =0, start =-1; // put -1 so that u can check if lastInstance > start . so tjat next time u make start = lastInstance . if u put 0 here it will be trouble u must check >= start and u cant makem start = lastInstance.  
+        for(int i=0;i<s.size();i++){
+            if(v[s[i]] > start) // if we had seen this char in the window alrd i.e after the start index
+                 start = v[s[i]]; // that becomes new start of window
+            len = max(len,i-start);// here if start == -1 then i+1 will be length eg bbbb = 4 
+            
+            v[s[i]]=i;
+    }
+    return len;
+    }
+};
+/*  start at 0 index
+ int lengthOfLongestSubstring(string s) {
         vector<int> v(256,-1); // initially lastSeenindex of all is -1 -> store char, lastSeenindex
         int len =0, start =0; // put -1 so that u can check if lastInstance > start . so tjat next time u make start = lastInstance . if u put 0 here it will be trouble u must check >= start and u cant makem start = lastInstance.  
         for(int i=0;i<s.size();i++){
@@ -22,5 +28,5 @@ public:
             v[s[i]]=i+1;
     }
     return len;
-    }
-};
+
+*/

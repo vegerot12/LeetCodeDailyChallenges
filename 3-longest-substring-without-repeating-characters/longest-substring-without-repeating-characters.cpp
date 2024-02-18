e88d5e76c5dbd199bbc731e1f6fbee9d90b7d435
vegerot12 <nandhinirajavell@gmail.com>
2024-02-18 18:48:21 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (56.33%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,23 +1,26 @@
 class Solution {
+    //     vector<int> m(256,-1); // stores last seen index of all chars 
+//         int start = -1, ml =0;
+        
+//         for(int i=0;i<s.size();i++){
+//             if(m[s[i]]>start)
+//                 start=m[s[i]];
+//             m[s[i]]=i;
+//             ml=max(ml,i-start);
+//         }
+//     return ml;
+    // O(N) and O(N)
 public:
     int lengthOfLongestSubstring(string s) {
-        int n = s.size();
-        if(n<=1) return n;
-        unordered_map<char, int> m;
-        int res = 0;
-        int start = 0, i=0;
-    
-
-        for( i = 0;i<n;i++){
-            if(m.find(s[i])!= m.end()  && m[s[i]] >= start){
-                res = max(res, i-start);
-                start = m[s[i]]+1; // next to this char prev occurence
-               
-            }
-             m[s[i]] = i;
+        vector<int> v(256,-1); // initially lastSeenindex of all is -1 -> store char, lastSeenindex
+        int len =0, start =0; // put -1 so that u can check if lastInstance > start . so tjat next time u make start = lastInstance . if u put 0 here it will be trouble u must check >= start and u cant makem start = lastInstance.  
+        for(int i=0;i<s.size();i++){
+            if(v[s[i]] >= start) // if we had seen this char in the window alrd i.e after the start index
+                 start = v[s[i]]; // that becomes new start of window
+            len = max(len,i-start+1);// here if start == -1 then i+1 will be length eg bbbb = 4 
             
-        }
-
-        return max(res, i-start); 
+            v[s[i]]=i+1;
+    }
+    return len;
     }
 };
